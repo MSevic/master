edc25d3776ed634f8f266c6f69b79314349c24e5
Mihailo <mihailosevic@gmail.com>
2021-03-06 19:44:39 +0100

    Frontend included

--- Broker/index.js
+++ Broker/index.js
@@ -2,11 +2,12 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
 // console.log(__dirname);
-// app.use(express.static(__dirname+'/public'));
+// app.use(express.static('../Frontend'));
 app.get('/', function (req, res) {
-    res.send({'hello world': "world"})
-    // res.sendfile(__dirname+'/public/registration.html');
+    // res.send({'hello world': "path.join(__dirname, '../Frontend', 'index.html'"})
+    res.sendFile(path.join(__dirname, '../Frontend', 'index.html'));
 });
 io.on('connection', function (socket) {
     console.log("New Connection")
