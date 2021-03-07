32a13c12aaca20afb88e4c9ebbf32e7dd632dbbd
Mihailo <mihailosevic@gmail.com>
2021-03-07 17:00:05 +0100

    Added frontend

--- broker/index.js
+++ broker/index.js
@@ -3,16 +3,20 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+var frontend = require('./sevices/forntend/service')
 // console.log(__dirname);
 // app.use(express.static('../Frontend'));
 app.get('/', function (req, res) {
     // res.send({'hello world': "path.join(__dirname, '../Frontend', 'index.html'"})
-    res.sendFile(path.join(__dirname, '../Frontend', 'index.html'));
+    // res.sendFile(path.join(__dirname, '../Frontend', 'index.html'));
+    frontend(res)
+    // res.send(result())
+    // res.send('123123');
 });
 io.on('connection', function (socket) {
     console.log("New Connection")
 });
 
-http.listen(8080, function () {
-    console.log('listening on *:8080');
+http.listen(8000, function () {
+    console.log('listening on *:8000');
 });
---
+++ broker/sevices/forntend/service.js
@@ -0,0 +1,4 @@
+frontend = async (callback) => {
+    callback.redirect('http://localhost:3000')
+};
+module.exports = frontend
